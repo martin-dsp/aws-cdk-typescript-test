c96e8f2f67823fa150c128c0ee5a56c37c2d92d1
martin-dsp <kcs19542001@hotmail.com>
2022-04-11 10:51:57 +0900

    Feat S3 upload successfully done by adding AdminAccess IAM

--- lib/aws-cdk-typescript-test-stack.ts
+++ lib/aws-cdk-typescript-test-stack.ts
@@ -1,16 +1,13 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_s3 as s3 } from "aws-cdk-lib";
 
 export class AwsCdkTypescriptTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTypescriptTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, "MyBucket", {
+      bucketName: "my-cdk-test-bucket",
+    });
   }
 }
